8ffd08ade0aac71548c62a86eff807fd27be8e68
Shane <shanep42@gmail.com>
2023-10-11 17:43:28 -0500

    Adds sign up/in functionality with Clerk

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold.mb-5">Welcome</h1>
+      <p className="mb-5">
+        This is a demo/tutorial site for Clerk using Next.js. Go ahead and sign up or sign in!
+      </p>
+    </>
+  )
+}
